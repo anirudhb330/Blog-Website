8752e073053ea83ff1999a78600190da17d54c3c
Anirudhb680 <anirudh33080@gmail.com>
2024-01-06 17:40:44 +0530

    Added MUI Login page

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import { Login } from './Components/Login';
+import { LoginPage } from './Components/Login';
 
 function App() {
   return (
     <div className="App">
-      
-      <Login/>
+      <LoginPage/>
     </div>
   );
 }
